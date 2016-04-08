3543fa361ad743ad8d6a713b091740b4bfde6f6d
berylramadhian <berylramadhian@gmail.com>
2016-04-08 08:27:13 +0700

    init commit

---
+++ src/sisfo_penjualan_batu/Account.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sisfo_penjualan_batu;
+
+/**
+ *
+ * @author Yorozuya
+ */
+public class Account {
+    
+}
